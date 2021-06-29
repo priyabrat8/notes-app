c572915e22fc14907907923b42516581a0ab8c93
Priyabrat <PriyabratSwain888@gmail.com>
2021-06-29 22:25:59 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+yargs.version("1.1.0")
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+
+        body: {
+            describe : 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNotes(argv.title,argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler() {
+        notes.listNotes()
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder : {
+        title : {
+            describe : 'Note title',
+            demandOption : true,
+            type : 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+});
+
+yargs.parse()
